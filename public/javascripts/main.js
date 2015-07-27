45a3652aba1815116c3fe27d6693ee7343166cba
Yiran Mao <yiranmao@gmail.com>
2015-07-27 20:28:45 +0800

    **UNSTABLE** Commit 15/07/27 - 初始化项目

---
+++ public/javascripts/main.js
@@ -0,0 +1,69 @@
+function redoLayout(){
+	var articles = $('.article');
+	for(var i = 0, l = articles.length; i < l; i++) {
+		redoLayoutArticle($(articles[i]));
+	}
+}
+
+function redoLayoutArticle(article){
+	var content = article.find('.container'),
+		bgImg = article.find('.background');
+
+	if(article.hasClass('article-full')) {
+		article.css({
+			height: window.innerHeight
+		});
+		// content.css({
+		// 	paddingTop: (article.innerHeight() - content.innerHeight())/2
+		// });
+	}
+
+	if(bgImg) {
+		var defer = $.Deferred();
+		defer.promise(bgImg);
+
+		(function resolveWhenImageHeaderLoaded() {
+			if(bgImg.width() && bgImg.height) {
+				defer.resolve();
+			}
+			else{
+				setTimeout(resolveWhenImageHeaderLoaded, 100)
+			}
+		})();
+
+		bgImg.done(function(){
+			var containerHeight = article.innerHeight(),
+				containerWidth = article.width(),
+				bgWidthHeightRatio = bgImg.width() / bgImg.height(),
+				bgTheoryHeight, 
+				bgTheoryWidth, 
+				bgRealWidth, 
+				bgRealHeight;
+
+			// Let the bgImg fill height first. Calculate the width in that case
+			bgTheoryHeight = containerHeight;
+			bgTheoryWidth = bgTheoryHeight * bgWidthHeightRatio;
+			// The theortical width filled the container, good to go
+			if(containerWidth <= bgTheoryWidth) {
+				bgRealWidth = bgTheoryWidth;
+				bgRealHeight = bgTheoryHeight;
+			}
+			// The theortical width is not wide enough to fill the container, reverse it.
+			else{
+				bgRealWidth = containerWidth;
+				bgRealHeight = bgRealWidth / bgWidthHeightRatio;
+			}
+			bgImg.css({
+				height: bgRealHeight,
+				width: bgRealWidth,
+				left: (containerWidth - bgRealWidth) / 2,
+				top: (containerHeight - bgRealHeight) / 2 
+			}).show();
+		});
+	}
+}
+
+$(document).ready(function(){
+	redoLayout();
+	$(window).resize(redoLayout);
+});
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', {});
+});
+router.get('/huabanche', function(req, res){
+	res.render('huabanche', {});
+});
+router.get('/contact', function(req, res){
+	res.render('contact', {});
+});
+router.get('/about', function(req, res){
+	res.render('about', {});
+});
+
+module.exports = router;
